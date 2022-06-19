e9d449c495c842de17385adb1e15b91022f3ac28
b33luo <b33luo@uwaterloo.ca>
2022-06-19 10:56:53 -0400

    Validation

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,18 +6,20 @@
 
 - https://www.youtube.com/watch?v=Lv3OhfcxjkA: React, Material UI "Contact us" Form
 - https://www.youtube.com/watch?v=tKApfSoDPgM: Using Material UI "Select" element
+- https://www.youtube.com/watch?v=ev6E5SextWE: Form validation via 'react-hook-form' library
 
 --------------------------------------- /\ References */
 
 import React from 'react';
-import { Typography, Card, CardContent, Grid, TextField, Button, Box, MenuItem } from '@material-ui/core';
+import { Typography, Card, CardContent, Grid, TextField, Button, Box, MenuItem, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
 
 const serverURL = ""; //enable for dev mode
 
-const MuiSelect = () => {
-  const [movie, setMovie] = React.useState('')
+const MovieSelection = () => {
+  const [movie, selectedMovie] = React.useState('')
   const handleChange = (event) => {
-    setMovie(event.target.value);
+    selectedMovie(event.target.value);
   };
 
   return (
@@ -42,7 +44,104 @@ const MuiSelect = () => {
   )
 }
 
-function App() {
+const ReviewTitle = () => {
+  const [title, enteredTitle] = React.useState('')
+  const handleChange = (event) => {
+    enteredTitle(event.target.value);
+  };
+  return (
+    <TextField
+      label="Title"
+      placeholder="Enter review title"
+      variant="outlined"
+      value={title}
+      onChange={handleChange}
+      fullWidth
+      required />
+  )
+}
+
+const ReviewBody = () => {
+
+  const [review, enteredReview] = React.useState('')
+  const handleChange = (event) => {
+    enteredReview(event.target.value);
+  };
+
+  return (
+    <TextField
+      label="Review"
+      inputProps={{ maxLength: 200 }}
+      multiline rows={4}
+      placeholder="Type your thoughts here (max. 200 char)"
+      variant="outlined"
+      value={review}
+      onChange={handleChange}
+      fullWidth
+      required />
+  )
+}
+
+const ReviewRating = () => {
+
+  const [rating, selectedRating] = React.useState('')
+  const handleChange = (event) => {
+    selectedRating(event.target.value);
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Rating</FormLabel>
+      <RadioGroup
+        row
+        aria-label="position"
+        name="position"
+        value={rating}
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="1"
+          control={<Radio color="primary" />}
+          label="1"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel
+          value="2"
+          control={<Radio color="primary" />}
+          label="2"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel
+          value="3"
+          control={<Radio color="primary" />}
+          label="3"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel
+          value="4"
+          control={<Radio color="primary" />}
+          label="4"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel
+          value="5"
+          control={<Radio color="primary" />}
+          label="5"
+          labelPlacement="bottom"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+function Review() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => console.log(data)
 
   return (
     <div className="App">
@@ -53,34 +152,31 @@ function App() {
       <Card style={{ maxWidth: 450, margin: "0 auto", padding: "20px 5px" }}>
         <CardContent>
 
-          <Typography gutterBottom variant="h5">
-            Placeholder
-          </Typography>
-
-          <form>
-            <Grid container spacing={1}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
 
               <Grid xs={12} item>
-                <MuiSelect />
+                <MovieSelection />
+              </Grid>
+              <Grid xs={12} item>
+                <ReviewTitle />
               </Grid>
 
-              <Grid xs={12} sm={6} item>
-                <TextField label="First Name" placeholder="Enter first name" variant="outlined" fullWidth required />
+              <Grid xs={12} item align="center">
+                <ReviewRating />
               </Grid>
-              <Grid xs={12} sm={6} item>
-                <TextField label="Last Name" placeholder="Enter last name" variant="outlined" fullWidth required />
+
+              <Grid xs={12} item>
+                <ReviewBody />
               </Grid>
               <Grid xs={12} item>
-                <TextField type="email" label="Email" placeholder="Enter email" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid xs={12} item>
-                <TextField type="number" label="Phone Number" placeholder="Enter phone number" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid xs={12} item>
-                <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid xs={12} item>
-                <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth>
+                  Share your review
+                </Button>
               </Grid>
 
             </Grid>
@@ -93,4 +189,4 @@ function App() {
 
 }
 
-export default App;
+export default Review;
